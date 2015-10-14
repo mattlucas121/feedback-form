56af470501fb7f378d00cf2283d944296ce290bb
Matt Lucas <mlucas@kwantera.com>
2015-10-14 13:49:41 -0400

    Form is now functional. Demo working.

--- demo/demo.js
+++ demo/demo.js
@@ -1,7 +1,17 @@
 angular.module('demo', ['feedback'])
 
   .controller('DemoController', function($scope) {
+    $scope.submitForm = function () {
+      console.log('Submitted form!');
+    };
+
     $scope.test = 'test';
+
+    $scope.formData = {
+      name: 'test1',
+      email: 'test2',
+      comment: 'test2'
+    };
   })
 
 ;
--- src/feedbackForm.js
+++ src/feedbackForm.js
@@ -1,14 +1,38 @@
 angular.module('feedback', [
+  'ngAnimate'
 ])
 
   .directive('feedbackForm', function() {
     return {
       restrict: 'A',
+      require: 'ngModel',
       scope: {
-        options: '&'
+        //options: '&',
+        ngModel: '=',
+        submitFunction: '&'
       },
       templateUrl: '../src/feedbackForm.html',
-      controller: function ($scope) {
+      controller: function ($scope, $element) {
+      },
+      link: function (scope) {
+        scope.settings = {
+          showForm: false
+        };
+
+        /**
+         * Toggles status of dialog form
+         * @param force Boolean optional, forces form dialog to given value
+         * @returns {*} New state of the dialog form
+         */
+        scope.toggleForm = function (force) {
+          if (angular.isDefined(force)) {
+            scope.settings.showForm = force;
+            return force;
+          }
+
+          scope.settings.showForm = !force;
+          return force;
+        };
 
       }
     };
